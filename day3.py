eca20e1dda2e1a62b0df0bb0d79a6822526c7e8c
John Berthels <john.berthels@gmail.com>
2018-12-03 13:23:30 +0000

    Implement psuedo-quadtree
    The implementation without depth was slower than the previous
    one-level-split-into-ten-steps approach. Adding a 'depth' to limit
    the nesting speeds it up.
    
    It's not a quadtree because we are in integer space, so can't
    always halve it. Instead, pick a smallish prime and branch that
    many times.

--- day3.py
+++ day3.py
@@ -8,33 +8,31 @@ def main():
     claims = [parse_claim_to_rect(line) for line in lines]
 
     print("Got {} claims".format(len(claims)))
-#    for claim in claims:
-#        print(claim)
-
-#    part1(claims)
-    part2(claims)
+    part1(claims)
+#    part2(claims)
 
 def part1(claims):
 
     width = 1000
     height = 1000
-    step = 100
+    whole_region = Rect(None, 0, 0, width, height)
+    depth = 2
+    print("Building...")
+    qt = QuadTree(depth, whole_region, claims)
+    print("Built...")
     overcommitted = 0
-    for i in range(0, width, step):
-        print("i: {}".format(i))
-        for j in range(0, height, step):
-            region = Rect(None, i, j, step, step)
-            region_rects = [r for r in claims if region.intersect(r) is not None]
-
-            for ii in range(0, step):
-                for jj in range(0, step):
-                    within = 0
-                    for rect in region_rects:
-                        if rect.contains(i+ii, j+jj):
-                            within += 1
-                            if within >= 2:
-                                overcommitted += 1
-                                break
+    for i in range(0, width):
+#            print("=== i, j: {},{}".format(i,j))
+        for j in range(0, height):
+#            print("=== i, j: {},{}".format(i,j))
+            rects = qt.rects_at(i, j)
+            within = 0
+            for rect in rects:
+                if rect.contains(i, j):
+                    within += 1
+                    if within >= 2:
+                        overcommitted += 1
+                        break
 
     print("overcommitted is {}".format(overcommitted))
 
@@ -44,6 +42,61 @@ def part2(claims):
         if len(intersects) == 1:
             print("does not intersect: {}".format(a))
         print("{} has {}".format(a.idx, len(intersects)))
+
+
+class QuadTree():
+
+
+    def __init__(self, depth, region, rects):
+        self.region = region
+        self.rects = rects
+        self.depth = 2
+        self._create_children()
+#        print("region {}".format(self))
+
+
+    def __repr__(self):
+        return "{} step ({},{})".format(self.region, self.w_step, self.h_step)
+
+
+    def _create_children(self):
+        self.children = None
+
+        if self.depth == 0:
+            return
+        small_primes = [7, 5, 3, 2]
+        self.w_step = 0
+        self.h_step = 0
+        for p in small_primes:
+            if self.region.width > p and self.region.width % p == 0:
+                self.w_step = self.region.width // p
+            if self.region.height > p and self.region.height % p == 0:
+                self.h_step = self.region.height // p
+        if self.w_step == 0 or self.h_step == 0:
+            return
+
+        self.children = []
+        for i in range(self.region.left, self.region.right(), self.w_step):
+            col = []
+            for j in range(self.region.top, self.region.bottom(), self.h_step):
+                region = Rect(None, i, j, self.w_step, self.h_step)
+                region_rects = [rect for rect in self.rects if region.intersect(rect) is not None]
+                col.append(QuadTree(self.depth - 1, region, region_rects))
+            self.children.append(col)
+
+
+    def rects_at(self, i, j):
+#        print("{}".format(self))
+#        print("rx i {} j {}".format(i, j));
+        if not self.region.contains(i, j):
+            raise RuntimeError("Point not in quadtree region")
+        if self.children is None:
+            return self.rects
+        isub = (i - self.region.left) // self.w_step
+        jsub = (j - self.region.top) // self.h_step
+#        print("rx isub {} jsub {}".format(isub, jsub));
+        return self.children[isub][jsub].rects_at(i, j)
+
 
 
 def parse_claim_to_rect(line):
@@ -60,6 +113,8 @@ class Rect():
 
     def __init__(self, idx, left, top, width, height):
         self.idx = idx
+        if self.idx is None:
+            self.idx = id(self)
         self.left = left
         self.top = top
         self.width = width
