ffa992a8fd74114ed592e5f654f064926ae60450
John Berthels <john.berthels@gmail.com>
2018-12-08 08:45:52 +0000

    day8: refactor to build tree of nodes

--- day8.py
+++ day8.py
@@ -3,44 +3,54 @@ import string
 
 def main():
 
-#    line = """2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2\n"""
-    with open("day8-input.txt") as f:
-        line = f.readline()
+    line = """2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2\n"""
+#    with open("day8-input.txt") as f:
+#        line = f.readline()
 
     part1(line)
 
+class Node:
 
-def read_tree_mdata(namei, tokens):
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+        self.mdata = []
+
+    def tree_sum_mdata(self):
+        return sum(self.mdata + [n.tree_sum_mdata() for n in self.children])
+
+def read_tree(namei, tokens):
     if not tokens:
         return 0, []
 
-#    name = next(namei)
+    name = next(namei)
+    node = Node(name)
+
 #    print("{}: {}".format(name, tokens))
 
     (num_children, num_node_mdata) = tokens[0:2]
     tokens = tokens[2:]
 
-    mdata_sum = 0
     for i in range(num_children):
-        child_mdata_sum, tokens = read_tree_mdata(namei, tokens)
-        mdata_sum += child_mdata_sum
+        child, tokens = read_tree(namei, tokens)
+        node.children.append(child)
 
     mdata = tokens[0:num_node_mdata]
-#    print("{}: M: {}".format(name, mdata))
-    mdata_sum += sum(mdata)
+    node.mdata += mdata
+
     tokens = tokens[num_node_mdata:]
 
-    return mdata_sum, tokens
+    return node, tokens
 
 def part1(line):
     line = line.rstrip()
 
     tokens = [int(s) for s in line.split(" ")]
-    namei = string.ascii_lowercase
+    namei = iter(string.ascii_lowercase)
 
-    total_mdata_sum, _ = read_tree_mdata(namei, tokens)
+    tree, _ = read_tree(namei, tokens)
 
-    print(total_mdata_sum)
+    print("Total metadata: {}".format(tree.tree_sum_mdata()))
 
 
 if __name__ == "__main__":
