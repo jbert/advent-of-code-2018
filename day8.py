3a0d54120e47cddcc92412c1d2d9f0ee59823bbc
John Berthels <john.berthels@gmail.com>
2018-12-08 09:28:23 +0000

    part2 - use tree structure to build value

--- day8.py
+++ day8.py
@@ -3,11 +3,23 @@ import string
 
 def main():
 
-    line = """2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2\n"""
-#    with open("day8-input.txt") as f:
-#        line = f.readline()
+#    line = """2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2\n"""
+    with open("day8-input.txt") as f:
+        line = f.readline()
 
-    part1(line)
+    line = line.rstrip()
+
+    tokens = [int(s) for s in line.split(" ")]
+
+    tree, _ = read_tree(make_names, tokens)
+
+    part1(tree)
+    part2(tree)
+
+
+def make_names():
+    yield from iter(string.ascii_lowercase)
+
 
 class Node:
 
@@ -19,11 +31,36 @@ class Node:
     def tree_sum_mdata(self):
         return sum(self.mdata + [n.tree_sum_mdata() for n in self.children])
 
-def read_tree(namei, tokens):
+    def value(self):
+        if self.children:
+            child_values = [self.children[i].value() for i in self._value_indices()]
+            return sum(child_values)
+        else:
+            return sum(self.mdata)
+
+    def _value_indices(self):
+            indices = [m-1 for m in self.mdata if m > 0 and m <= len(self.children)]
+#            print("M: {} lc: {} I: {}".format(self.mdata, len(self.children), indices))
+            return indices
+
+    def print(self):
+        self._print(0)
+
+    def _print(self, depth):
+        prefix = " " * (depth * 2)
+        print("{}{}".format(prefix, self))
+        for c in self.children:
+            c._print(depth+1)
+
+    def __repr__(self):
+        return "C: {} M: {} V: {} VI: {}".format(len(self.children), self.mdata, self.value(), self._value_indices())
+
+
+def read_tree(namer, tokens):
     if not tokens:
         return 0, []
 
-    name = next(namei)
+    name = namer()
     node = Node(name)
 
 #    print("{}: {}".format(name, tokens))
@@ -32,7 +69,7 @@ def read_tree(namei, tokens):
     tokens = tokens[2:]
 
     for i in range(num_children):
-        child, tokens = read_tree(namei, tokens)
+        child, tokens = read_tree(namer, tokens)
         node.children.append(child)
 
     mdata = tokens[0:num_node_mdata]
@@ -42,15 +79,12 @@ def read_tree(namei, tokens):
 
     return node, tokens
 
-def part1(line):
-    line = line.rstrip()
-
-    tokens = [int(s) for s in line.split(" ")]
-    namei = iter(string.ascii_lowercase)
-
-    tree, _ = read_tree(namei, tokens)
-
+def part1(tree):
     print("Total metadata: {}".format(tree.tree_sum_mdata()))
+
+def part2(tree):
+#    tree.print()
+    print("Value : {}".format(tree.value()))
 
 
 if __name__ == "__main__":
