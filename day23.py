f68e8c75ec3c4b2dfc4164c787d8b91fffdfc6e1
John Berthels <john.berthels@gmail.com>
2018-12-23 22:08:31 +0000

    Seems hard !

--- day23.py
+++ day23.py
@@ -32,6 +32,10 @@ def part1(bots):
 
 
 def part2(bots):
+    weakest_bot = min(bots, key=lambda b: b.r)
+    print("weakest bot: {}".format(weakest_bot))
+    min_step = weakest_bot.r
+
     xmin = 0
     xmax = 0
     ymin = 0
@@ -53,45 +57,77 @@ def part2(bots):
         if b.z > zmax:
             zmax = b.z
 
-    
-    while True:
-        steps = 10
-        grid_best = None
-        grid_best_count = 0
-        xstep = (xmax-xmin) // steps
-        if xstep < 1:
-            break
-        ystep = (ymax-ymin) // steps
-        zstep = (zmax-zmin) // steps
-        for x in range(xmin, xmax+1, xstep):
-            for y in range(ymin, ymax+1, ystep):
-                for z in range(zmin, zmax+1, zstep):
-                    count = len([b for b in bots if b.coords_in_range(x, y, z)])
-                    if count > grid_best_count:
-                        grid_best = (x, y, z)
-                        grid_best_count = count
-        print("xstep {} grid best {} count {}".format(xstep, grid_best, grid_best_count))
+    # Assumption: max score is acheived on an edge
+    edge_pts = []
 
-        xmin = grid_best[0] - (steps // 3) * xstep
-        xmax = grid_best[0] + (steps // 3) * xstep
-        ymin = grid_best[0] - (steps // 3) * ystep
-        ymax = grid_best[0] + (steps // 3) * ystep
-        zmin = grid_best[0] - (steps // 3) * zstep
-        zmax = grid_best[0] + (steps // 3) * zstep
+    best_pts = None
+    best_count = 0
 
-                
-#
-    #print("xmin {} xmax {} ymin {} ymax {} zmin{} zmax {}".format(xmin, xmax, ymin, ymax, zmin, zmax))
-    #for x in range(xmin, xmax+1):
-        #for y in range(ymin, ymax+1):
-            #for z in range(zmin, zmax+1):
+    def score(pt):
+        return len([b for b in bots if b.range_includes_pt(pt)])
 
-#    xorder = sorted(bots, key=lambda b: b.x)
-#    xchanges = [(b.x-b.r, b.x+b.r) for b in bots]
-#    ychanges = [(b.y-b.r, b.y+b.r) for b in bots]
-#    zchanges = [(b.z-b.r, b.z+b.r) for b in bots]
-#    print(xchanges)
+    for b in bots:
+        edge_pts = b.edge_points()
+        print("B {} nep {}".format(b, edge_pts))
+        for pt in edge_pts:
+            count = score(pt)
+            print("Best: {} {}".format(best_count, best_pts))
+            if count == best_count:
+                best_pts.append(pt)
+            elif count > best_count:
+                best_count = count
+                best_pts = [pt]
 
+    def dist(pt):
+        return abs(pt.x)+abs(pt.y)+abs(pt.z)
+
+    for v in best_vertices:
+        print("best count {} at {} d {}".format(best_count, v, abs(v.x)+abs(v.y)+abs(v.z)))
+
+    assert len(best_vertices) == 1
+
+    v = best_vertices[0]
+    assert v.x > 0
+    assert v.y > 0
+    assert v.z > 0
+    best_d = dist(v)
+    for _ in range(10):
+        w = Pt(v.x-1,v.y-1,v.z-1)
+        s = score(w)
+        print("w {} score {} d {} w in vert: {}".format(w, s, dist(w), v in vertices))
+#        if s == best_count:
+        v = w
+#        else:
+#            break
+
+    print("Best current v is {} d {}".format(v, dist(v)))
+        
+
+
+class Pt:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return "<{},{},{}>".format(self.x, self.y, self.z)
+
+    def adjacent(self):
+        return [
+                Pt(self.x - 1, self.y, self.z),
+                Pt(self.x + 1, self.y, self.z),
+                Pt(self.x, self.y - 1, self.z),
+                Pt(self.x, self.y + 1, self.z),
+                Pt(self.x, self.y, self.z - 1),
+                Pt(self.x, self.y, self.z + 1),
+                ]
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
 
 
 
@@ -108,11 +144,42 @@ class Bot:
     def coords_in_range(self, x, y, z):
         return abs(self.x - x) + abs(self.y - y) + abs(self.z - z) <= self.r
 
+    def range_includes_pt(self, pt):
+        return abs(self.x - pt.x) + abs(self.y - pt.y) + abs(self.z - pt.z) <= self.r
+
     def range_includes(self, other):
         return self.distance_to(other) <= self.r
 
+    def distance_to_pt(self, pt):
+        return abs(self.x - pt.x) + abs(self.y - pt.y) + abs(self.z - pt.z)
+
     def distance_to(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y) + abs(self.z - other.z)
+
+    def edge_points(self):
+        r = self.r
+        pts = []
+        for i in range(r+1):
+            pts.append(Pt(self.x+r-i, self.y+r-i, self.z + i))
+            pts.append(Pt(self.x-r+i, self.y+r-i, self.z + i))
+            pts.append(Pt(self.x+r+i, self.y-r+i, self.z + i))
+            pts.append(Pt(self.x-r+i, self.y-r+i, self.z + i))
+            pts.append(Pt(self.x+r-i, self.y+r-i, self.z - i))
+            pts.append(Pt(self.x-r+i, self.y+r-i, self.z - i))
+            pts.append(Pt(self.x+r+i, self.y-r+i, self.z - i))
+            pts.append(Pt(self.x-r+i, self.y-r+i, self.z - i))
+        return pts
+
+    def vertices(self):
+        r = self.r
+        return [
+                Pt(self.x - r, self.y, self.z),
+                Pt(self.x + r, self.y, self.z),
+                Pt(self.x, self.y - r, self.z),
+                Pt(self.x, self.y + r, self.z),
+                Pt(self.x, self.y, self.z - r),
+                Pt(self.x, self.y, self.z + r),
+                ]
 
 def parse_lines(lines):
     pattern = r'^pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)'
