c1ee4a0ff89ebdb201122789d067a886905ce076
John Berthels <john.berthels@gmail.com>
2019-01-04 08:31:09 +0000

    Assuming no cycles
    Wrong answer 4491 - too low

--- day20.py
+++ day20.py
@@ -2,9 +2,11 @@
 import sys
 
 def main():
-    #line = '^WNE$'
+    line = '^WNE$'
     sys.setrecursionlimit(100000)
     line = '^ENWWW(NEEE|SSE(EE|N))$'
+
+    line = '^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$'
 
     with open("day20-input.txt") as f:
         line = f.readline()
@@ -16,27 +18,64 @@ def main():
 def part1(r):
     m = Map(r)
     print(m)
-    print(m.distance())
+    print("Map distance: {}".format(m.distance()))
 
 
 class Map:
     def __init__(self, r):
         self.r = r
+        self._build()
 
     def distance(self):
         return 0
 
+    def _build(self):
+        rooms = dict()
+
+        max_len = 0
+        def _f(pos):
+            if rooms.get(pos.location()):
+                return
+            rooms[pos.location()] = True
+            pos.plen += 1
+            nonlocal max_len
+            if pos.plen > max_len:
+                max_len = pos.plen
+            print("P: {} ML: {}".format(pos, max_len))
+
+        p = Pos(0, 0, _f)
+        _f(p)
+        self.r.walk(p)
+
+        self.max_len = max_len - 1
+
+    def distance(self):
+        return self.max_len
+
+
+class Room:
+    def __init__(self, x, y):
+        self.n = None
+        self.e = None
+        self.s = None
+        self.w = None
+
 
 class Regex:
-    def __init__(self, toplevel):
+    def __init__(self, is_toplevel):
         self.r = []
-        self.toplevel = toplevel
+        self.is_toplevel = is_toplevel
 
     def __repr__(self):
         rstr = "".join([str(sr) for sr in self.r])
-        if self.toplevel:
+        if self.is_toplevel:
             rstr = '^' + rstr + '$'
         return rstr
+
+    def walk(self, p):
+        for sr in self.r:
+            sr.walk(p)
+
 
 class Literal:
     def __init__(self, s):
@@ -45,6 +84,19 @@ class Literal:
     def __repr__(self):
         return self.s
 
+    def walk(self, p):
+        for c in self.s:
+            if c == 'N':
+                p.n()
+            elif c == 'E':
+                p.e()
+            elif c == 'W':
+                p.w()
+            elif c == 'S':
+                p.s()
+            else:
+                raise RuntimeError("wtf")
+
 
 class Option():
     def __init__(self):
@@ -52,6 +104,54 @@ class Option():
 
     def __repr__(self):
         return "(" + "|".join([str(so) for so in self.o]) + ")"
+
+    def walk(self, p):
+        p.save()
+        for so in self.o:
+            so.walk(p)
+            p.restore()
+
+
+class Pos():
+    def __init__(self, x, y, f, plen=0):
+        self.x = x
+        self.y = y
+        self.f = f
+        self.plen = plen
+
+    def n(self):
+        self.y -= 1
+        self.f(self)
+
+    def s(self):
+        self.y += 1
+        self.f(self)
+
+    def w(self):
+        self.x -= 1
+        self.f(self)
+
+    def e(self):
+        self.x += 1
+        self.f(self)
+
+    def __repr__(self):
+        return "{},{} ({})".format(self.x, self.y, self.plen)
+
+    def location(self):
+        return "{},{}".format(self.x, self.y)
+
+    def save(self):
+        print("SAVE: plen {}".format(self.plen))
+        self._save_x = self.x
+        self._save_y = self.y
+        self._save_plen = self.plen
+
+    def restore(self):
+        self.x = self._save_x
+        self.y = self._save_y
+        self.plen = self._save_plen
+        print("REST: plen {}".format(self.plen))
 
 
 def parse_regex(line):
