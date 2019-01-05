51234cca03ac4cfebdf9175e5197d0beaaab749a
John Berthels <john.berthels@gmail.com>
2019-01-05 15:03:17 +0000

    Running algo but wrong answer for test

--- day21.py
+++ day21.py
@@ -1,16 +1,114 @@
 #!/usr/bin/python3
+import heapq
+
 
 def main():
     depth = 510
-    target = (10,10)
+    target = (10, 10)
 
-    depth = 5616
-    target = (10,785)
+#    depth = 5616
+#    target = (10, 785)
 
     system = make_system(depth, target)
     print_system(depth, target, system)
     answer = risk_level(depth, target, system)
     print("Part1: {}".format(answer))
+
+    target = target[0] + target[1] * 1j
+    path = find_path(depth, target, system)
+    print("Part2: {} minutes".format(path.minutes))
+    node = path
+    while node:
+        print(node)
+        node = node.prev
+
+
+def find_path(depth, target, system):
+    # We run A*, but consider each tooling option as a separate path
+    # with the cost of switching built into the heuristic
+
+    class Node:
+        def __init__(self, pt, tool, prev):
+            self.pt = pt
+            self.tool = tool
+
+            self.prev = prev
+            self.minutes = 0
+            t = 1
+            if prev:
+                if self.tool != prev.tool:
+                    t += 7
+                self.minutes = prev.minutes + t
+            self.f = abs(pt.real - target.real) + abs(pt.imag - target.imag) + self.minutes
+
+        def __lt__(self, other):
+            if self.f < other.f:
+                return True
+            if self.f > other.f:
+                return False
+            if self.pt.real < other.pt.real:
+                return True
+            return self.pt.imag < other.pt.imag
+
+        def __eq__(self, other):
+            return self.pt == other.pt and self.tool == other.tool
+
+        def __hash__(self):
+            return hash((self.pt, self.tool))
+
+        def __repr__(self):
+            return "{}: {} {} mins".format(self.pt, self.tool, self.minutes)
+
+        def adjacent_pts(self):
+            adj = [self.pt + 1, self.pt - 1, self.pt + 1j, self.pt - 1j]
+            return [a for a in adj if a.real >= 0 and a.imag >= 0]
+
+    def _viable(n):
+        gindex = system[int(n.pt.imag)][int(n.pt.real)]
+        t = gindex_to_elevel(depth, gindex) % 3
+        if t == 0:
+            # rocky
+            return n.tool != 'none'
+        elif t == 1:
+            # wet
+            return n.tool != 'torch'
+        elif t == 2:
+            # narrow
+            return n.tool != 'gear'
+        else:
+            raise RuntimeError("wtf")
+
+    # Don't go back where we've been
+    visited = set()
+    start = Node(0+0j, 'torch', None)
+    visited.add(start)
+    # Edge we are exploring
+    fringe = [start]
+
+    # Kept as a priority queue (sorted by the A-star function 'f' in Node above)
+    heapq.heapify(fringe)
+    while True:
+        if len(fringe) <= 0:
+            # No path
+            return None
+
+        node = heapq.heappop(fringe)
+#        print("N: {}".format(node))
+        if node.pt == target:
+            # Found it
+            break
+        next_steps = [Node(next_pt, tool, node) for tool in ['none', 'torch', 'gear'] for next_pt in node.adjacent_pts()]
+        next_steps = [ns for ns in next_steps if _viable(ns)]
+        print("NS: {}".format(next_steps))
+        for next_step in next_steps:
+            if next_step in visited:
+                continue
+            heapq.heappush(fringe, next_step)
+#            print("A: {}".format(next_step.pt))
+            visited.add(next_step)
+
+    assert node.pt == target
+    return node
 
 
 def risk_level(depth, target, system):
@@ -42,12 +140,12 @@ def make_system(depth, target):
     tx, ty = target
 
     gindex = []
-    for y in range(ty + 1):
+    for y in range(3 * ty + 1):
         row = []
         gindex.append(row)
 #        print("Y: {}".format(y))
-        for x in range(tx + 1):
-#            print("X: {}".format(x))
+        for x in range(3 * tx + 1):
+            # print("X: {}".format(x))
             if x == 0 and y == 0:
                 row.append(0)
             elif x == tx and y == ty:
